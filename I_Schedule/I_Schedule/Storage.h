1e6684370dfd6d46be13fc1344ab8ac5276180be
Conrad <a0099303@u.nus.edu>
2015-02-26 15:50:16 +0800

    Storage class with modified header

--- I_Schedule/I_Schedule/Storage.h
+++ I_Schedule/I_Schedule/Storage.h
@@ -1,8 +1,20 @@
 #pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Storage
 {
 public:
 	Storage();
+	Storage(string filename);
 	~Storage();
+
+private:
+	string _filename;
+	vector<string> loadFile();
+	string writeToFile();
+	string clearFile();
+
 };
 
