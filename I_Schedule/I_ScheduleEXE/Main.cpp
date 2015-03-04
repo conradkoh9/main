23a040a92b94f3a16d598d00c9e3ee2e7adb8d34
Conrad <a0099303@u.nus.edu>
2015-03-04 16:39:49 +0800

    Lib Test Properly Linked, Storage Unit Test Added

--- I_Schedule/I_ScheduleEXE/Main.cpp
+++ I_Schedule/I_ScheduleEXE/Main.cpp
@@ -5,25 +5,7 @@ using namespace std;
 void printVect(vector<string> input);
 
 int main(){
-	Storage* storage = new Storage();
-	cout << "created storage" << endl;
-	vector<string> all_feedback;
-	string feedback;
-	feedback = storage->clearFile();
-	all_feedback.push_back(feedback);
-	feedback = storage->writeToFile("hello world");
-	all_feedback.push_back(feedback);
-	feedback = storage->loadFile();
-	all_feedback.push_back(feedback);
 	
-	vector<string>::iterator iter;
-	vector<string> content = storage->getContent();
-	for (iter = content.begin(); iter != content.end(); ++iter){
-		cout << "content: " << *iter << endl;
-	}
-
-	printVect(all_feedback);
-
 	system("pause");
 	return 0;
 }
--- I_Schedule/I_ScheduleLibraryTest/Libtest.cpp
+++ I_Schedule/I_ScheduleLibraryTest/Libtest.cpp
@@ -1,17 +1,56 @@
 #include "stdafx.h"
+#include "Storage.h"
 #include "CppUnitTest.h"
 
+using namespace std;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace I_ScheduleLibraryTest
-{		
-	TEST_CLASS(I_ScheduleLibraryTest)
+{	Storage* storage = new Storage();
+	
+	TEST_CLASS(myLibTest)
 	{
 	public:
 		
 		TEST_METHOD(Storage)
 		{
-			// TODO: Your test code here
+			const string _FEEDBACK_GENERIC_SUCCESS = "STORAGE SUCCESS";
+			const string _FEEDBACK_GENERIC_FAILURE = "STORAGE FAILED";
+			const string _FEEDBACK_LOAD_SUCCESS = "LOAD SUCCESS";
+			const string _FEEDBACK_LOAD_FAILURE = "LOAD FAILED";
+			const string _FEEDBACK_WRITE_SUCCESS = "WRITE SUCCESS";
+			const string _FEEDBACK_WRITE_FAILURE = "WRITE FAILED";
+			const string _FEEDBACK_CLEAR_SUCCESS = "CLEAR SUCCESS";
+			const string _FEEDBACK_CLEAR_FAILURE = "CLEAR FAILED";
+			const string _FEEDBACK_FILE_EMPTY = "FILE EMPTY";
+			const string _EMPTY_STRING = "";
+
+			vector<string> expected_all_feedback;
+			expected_all_feedback.push_back("hello world");
+
+			vector<string> all_feedback;
+			string feedback;
+			feedback = storage->clearFile();
+			Assert::AreEqual(feedback, _FEEDBACK_CLEAR_SUCCESS);
+			all_feedback.push_back(feedback);
+			feedback = storage->writeToFile("hello world");
+			Assert::AreEqual(feedback, _FEEDBACK_WRITE_SUCCESS);
+			all_feedback.push_back(feedback);
+			feedback = storage->loadFile();
+			Assert::AreEqual(feedback, _FEEDBACK_LOAD_SUCCESS);
+			all_feedback.push_back(feedback);
+
+			vector<string>::iterator iter, iter2;
+			iter2 = expected_all_feedback.begin();
+			vector<string> content = storage->getContent();
+			for (iter = content.begin(); iter != content.end(); ++iter){
+				Assert::AreEqual(*iter ,*iter2);
+				++iter2;
+			}
+
+			//Assert::AreEqual(all_feedback, expected_all_feedback);
+			
+			 
 		}
 
 	};
